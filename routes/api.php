1cb9069ba0dbb3f491abd104de002c197d93cd8b
Nicolas NGUYEN <nngvk@studec.fr>
2022-04-25 15:03:20 +0200

    Back form

---
+++ routes/api.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::middleware('auth:sanctum')-> get('/user',function (Request $request) {
+    return $request-> user();
+});
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+Route::middleware('auth:sanctum')->get('/user/revoke', function (Request $request) {
+
+    $user = $request-> user();
+    $user->tokens()->delete();
+    return 'token are deleted';
+});
+
+
+Route::post('/auth/register', [AuthController::class, 'register']);
